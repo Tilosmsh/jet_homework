5ef011d7d5af4dc60bc7b6190603b27006116ed2
Tilosmsh <lzy6078@hotmail.com>
2024-04-17 16:36:40 +0300

    added web page

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-    const postcode = 'EC4M7RF'; // Example postcode
-    fetch(`http://localhost:5000/restaurants/${postcode}`)  // Adjust URL based on where your Flask app is hosted
+    const postcode = 'EC4M 7RF'; // Example postcode
+    fetch(`https://jet-hommework.ew.r.appspot.com/restaurants/${postcode}`)
         .then(response => response.json())
         .then(data => displayRestaurants(data))
         .catch(error => console.error('Error fetching data:', error));
 });
+
+function displayRestaurants(restaurants) {
+    const list = document.getElementById('restaurant-list');
+    restaurants.forEach(restaurant => {
+        const div = document.createElement('div');
+        div.className = 'restaurant';
+        div.innerHTML = `<h2>${restaurant.Name}</h2>
+                         <p>Cuisines: ${restaurant.Cuisines}</p>
+                         <p>Rating: ${restaurant.Rating}</p>
+                         <p>Address: ${restaurant.Address}</p>`;
+        list.appendChild(div);
+    });
+}
